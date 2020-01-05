9f9c82ee916d78a1fd185fc61b4b0f0b196b90f5
Mikael Carlavan <contact@mika-carl.fr>
2020-01-05 21:59:43 +0100

    Create StreamObject.php
    Add stream object

---
+++ src/Base/StreamObject.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Papier\Base;
+
+use Papier\Base\Object;
+use Papier\Base\IntegerObject;
+use Papier\Base\DictionaryObject;
+use Exception;
+
+class StreamObject extends Object
+{
+    /**
+     * The content of the object.
+     *
+     * @var mixed
+     */
+    protected $stream;
+    
+    /**
+     * Create a new ArrayObject instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->value = new DictionaryObject();
+        parent::__construct();
+    } 
+
+    /**
+     * Get object's dictionary.
+     *
+     * @return string
+     */
+    protected function getDictionary()
+    {
+        return $this->getValue();
+    }
+
+    /**
+     * Get object's stream.
+     *
+     * @return string
+     */
+    protected function getStream()
+    {
+        return $this->stream;
+    }
+
+    /**
+     * Set object's stream.
+     *  
+     * @param  mixed  $stream
+     * @return \Papier\StreamObject
+     */
+    protected function setStream($stream)
+    {
+        $this->stream = $stream;
+        return $this;
+    } 
+
+    /**
+     * Add entry to stream's dictionnary.
+     *      
+     * @param  string  $key
+     * @param  mixed  $object
+     * @return \Papier\StreamObject
+     */
+    protected function addEntry($key, $object)
+    {
+        $this->getDictionary()->setObjectForKey($key, $object);
+        return $this;
+    } 
+
+    /**
+     * Format object's value.
+     *
+     * @return string
+     */
+    public function format()
+    {
+        $dictionary = $this->getDictionary();
+        $stream = $this->getStream();
+        $stream = $stream ? $stream->format() : '';
+
+        // Compute length of stream and set it into dictionary
+        try {
+            $length = strlen($stream);
+            $length = new IntegerObject();
+            $length->setValue(intval($length));
+    
+            $dictionary->addEntry('Length', $length);
+        } catch (Exception $e) {
+            throw $e;
+        }
+
+        $value = '';
+        if ($dictionary) {
+            $value .= $dictionary->format() . $this->EOL_MARKER;
+        }
+        $value .= 'stream' . $this->EOL_MARKER;
+        $value .= $stream ? $stream . $this->EOL_MARKER : '';
+        $value .= 'endstream';
+
+        return $value;
+    }
+}
