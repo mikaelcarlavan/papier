1a319273b07560b56954009b4522c8352d1644f7
Mikael Carlavan <contact@mika-carl.fr>
2020-01-05 22:00:37 +0100

    Update IntValidator.php
    Update documentation.
    Add min and max optional parameters for validation

--- src/Validator/IntValidator.php
+++ src/Validator/IntValidator.php
@@ -6,14 +6,22 @@ use Papier\Validator\Base\Validator;
 class IntValidator implements Validator
 {
      /**
-     * Test if given string is a valid integer.
+     * Test if given parameter is a valid integer.
      * 
      * @param  mixed  $value
+     * @param  int  $min
+     * @param  int  $max
      * @return bool
      */
-    public static function isValid($value)
+    public static function isValid($value, $min = null, $max = null)
     {
-        $isValid = is_int($value);         
+        $isValid = is_int($value);
+        if (is_int($min) && $isValid) {
+            $isValid = $isValid & ($value >= $min);
+        }  
+        if (is_int($max) && $isValid) {
+            $isValid = $isValid & ($value <= $max);
+        }             
         return $isValid;
     }
 }
