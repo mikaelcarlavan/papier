4979e320dacbbbb0972e1aff20210ba98eccd8bb
Mikael Carlavan <contact@mika-carl.fr>
2020-06-28 18:01:54 +0200

    Create StringObject.php

---
+++ src/Object/Base/StringObject.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Papier\Object\Base;
+
+use Papier\Object\Base\Object;
+use Papier\Validator\StringValidator;
+
+use InvalidArgumentException;
+
+class StringObject extends IndirectObject
+{
+    /**
+    * Set object's value.
+    *
+    * @param  mixed  $value
+    * @throws InvalidArgumentException if the provided argument is not of type 'string'.
+    * @return \Papier\Object\Base\StringObject
+    */
+    public function setValue($value)
+    {
+        if (!StringValidator::isValid($value)) {
+            throw new InvalidArgumentException("String is incorrect. See String class's documentation for possible values.");
+        }
+
+        return parent::setValue($value);
+    } 
+}
