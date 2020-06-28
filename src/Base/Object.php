541e1ef588983a38c7a4a61b1e49a4276ad197fd
Mikael Carlavan <contact@mika-carl.fr>
2020-06-28 21:24:34 +0200

    Move object abstract class in base directory

---
+++ src/Base/Object.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Papier\Base;
+
+use Papier\Base\Comment;
+
+abstract class Object
+{
+    /**
+     * The value of the object.
+     *
+     * @var mixed
+     */
+    protected $value;
+
+    /**
+     * End-of-line marker.
+     *
+     * @var string
+     */
+    const EOL_MARKER = "\r\n"; 
+
+    /**
+     * Magical method.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->write();
+    }
+
+    /**
+     * Format object's value.
+     *
+     * @return string
+     */
+    public function format()
+    {
+        return null;
+    }
+
+    /**
+     * Get object's value.
+     *
+     * @return string
+     */
+    protected function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Set object's value.
+     *  
+     * @param  mixed  $value
+     * @return \Papier\Object\Base\Object
+     */
+    protected function setValue($value)
+    {
+        $this->value = $value;
+        return $this;
+    } 
+
+    /**
+     * Write object's value.
+     *
+     * @return string
+     */
+    public function write()
+    {
+        $value = $this->format(). $this->EOL_MARKER;
+        return $value;
+    }
+}
