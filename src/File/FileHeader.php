bfb9b9b694ddc088e329eca8ee7c3e559c07f6fa
Mikael Carlavan <contact@mika-carl.fr>
2020-06-29 07:34:29 +0200

    Update FileHeader.php
    Add method to specifiy if file contains binary data

--- src/File/FileHeader.php
+++ src/File/FileHeader.php
@@ -4,6 +4,8 @@ namespace Papier\File;
 
 use Papier\Base\Object;
 use Papier\Validator\IntValidator;
+use Papier\Validator\BoolValidator;
+
 
 use InvalidArgumentException;
 
@@ -24,14 +26,20 @@ class FileHeader extends Object
     private $maxVersion = 7;
 
     /**
+     * Bool which indicates if file has binary data.
+     *
+     * @var bool
+     */
+    private $hasBinaryData = true;  
+    /**
      * Format header's content.
      *
      * @return string
      */
-    public function format($hasBinaryData = true)
+    public function format()
     {
         $value = sprintf("%%PDF-1.%d", $this->getVersion());
-        if ($hasBinaryData) {
+        if ($this->getHasBinaryData()) {
             $comment = new Comment();
             $chars = array_map('chr', range(128, 131));
             $value .= $comment->setValue(implode('', $chars))->format();
@@ -62,5 +70,31 @@ class FileHeader extends Object
         }
 
         return parent::setValue($value);
+    } 
+
+    /**
+     * Get if file has binary data.
+     *
+     * @return int
+     */
+    protected function getHasBinaryData()
+    {
+        return $this->hasBinaryData;
+    }
+
+    /**
+     * Set if file has bianry data.
+     *  
+     * @param  bool  $hasBinaryData
+     * @return \Papier\File\FileHeader
+     */
+    protected function setHasBinaryData($hasBinaryData)
+    {
+        if (!BoolValidator::isValid($hasBinaryData)) {
+            throw new InvalidArgumentException("HasBinaryData is incorrect. See FileHeader class's documentation for possible values.");
+        }
+
+        $this->hasBinaryData = $hasBinaryData;
+        return $this;
     } 
 }
