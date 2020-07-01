67c03c0fac78af1a824aae20e7616c0cca61754d
Mikael Carlavan <contact@mika-carl.fr>
2020-07-01 19:03:12 +0200

    Update CrossReferenceSubsection.php

---
+++ src/File/CrossReference/CrossReferenceSubsection.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Papier\File\CrossReference;
+
+use Papier\Object\ArrayObject;
+use Papier\File\CrossReference\CrossReferenceEntry;
+
+class CrossReferenceSubsection extends ArrayObject
+{
+    /**
+     * Magical method.
+     *  
+     */
+    public function __set($key, $object)
+    {
+        if (!$object instanceof CrossReferenceEntry) {
+            throw new InvalidArgumentException("Cross-reference entry is incorrect. See ".get_class($this)." class's documentation for possible values.");
+        }
+        $this->setObjectForKey($key, $object);
+    }
+
+    /**
+     * Magical method.
+     *  
+     */
+    public function __get($key)
+    {
+        $this->getObjectForKey($key);
+    }
+
+    /**
+     * Add new subsection.
+     *  
+     * @return \Papier\Object\CrossReferenceEntry
+     */
+    public function addEntry()
+    {
+        $objects = $this->getEntries();
+
+        $object = new CrossReferenceEntry();
+        $objects[] = $object;
+
+        $this->setObjects($objects);
+
+        return $object;
+    }
+
+    /**
+     * Get subsections.
+     *  
+     * @return array
+     */
+    public function getEntries()
+    {
+        $objects = $this->getObjects();
+        return $objects;
+    }  
+
+    /**
+     * Format object's value.
+     *
+     * @return string
+     */
+    public function format()
+    {
+        $objects = $this->getEntries();
+        $firstNumber = 0;
+        if (is_array($objects) && count($objects) > 0) {
+            $firstNumber = $objects[0]->getNumber();
+        }
+        $value = sprintf("%d %d", $firstNumber, count($objects));
+        return $value;
+    }
+}
