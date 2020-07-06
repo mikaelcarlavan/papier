5eab4c6218f9c4f4ee52a1a4c67468aaf753125b
Mikael Carlavan <contact@mika-carl.fr>
2020-07-06 07:33:58 +0200

    Update PageLayout.php
    Add documentation

--- src/Document/PageLayout.php
+++ src/Document/PageLayout.php
@@ -13,42 +13,42 @@ use Exception;
 class PageLayout extends Object
 {
     /**
-     * Single page layout
+     * Single page layout (display one page at a time)
      *
      * @var string
      */
     const SINGLE_PAGE_LAYOUT = 'SinglePage';
   
     /**
-     * One column layout
+     * One column layout (display the page in one column)
      *
      * @var string
      */
     const ONE_COLUMN_LAYOUT = 'OneColumn';
 
     /**
-     * Two column left layout
+     * Two column left layout (display the pages in tow columns, with odd-numbered pages on the left)
      *
      * @var string
      */
     const TWO_COLUMN_LEFT_LAYOUT = 'TwoColumnLeft';
 
     /**
-     * Two column right layout
+     * Two column right layout (display the pages in tow columns, with odd-numbered pages on the right)
      *
      * @var string
      */
     const TWO_COLUMN_RIGHT_LAYOUT = 'TwoColumnRight';
 
     /**
-     * Two page left layout
+     * Two page left layout (display the pages two at a time, with odd-numbered pages on the left)
      *
      * @var string
      */
     const TWO_PAGE_LEFT_LAYOUT = 'TwoPageLeft';
 
     /**
-     * Two page right layout
+     * Two page right layout (display the pages two at a time, with odd-numbered pages on the right)
      *
      * @var string
      */
