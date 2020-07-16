577a0721c69570b04a4fb5da22b8d7388152037d
Mikael Carlavan <contact@mika-carl.fr>
2020-07-16 22:37:48 +0200

    Update TextStringType.php

--- src/Type/TextStringType.php
+++ src/Type/TextStringType.php
@@ -6,5 +6,14 @@ use Papier\Type\StringType;
 
 class TextStringType extends StringType
 {
- 
+    /**
+     * Get object's value.
+     *
+     * @return string
+     */
+    protected function getValue()
+    {
+        $value = parent::getValue();
+        return mb_convert_encoding($value, 'UTF-16BE');
+    }
 }
