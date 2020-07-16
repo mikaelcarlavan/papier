5f9d27e4be4f38c563958c726822aba40eebbaa4
Mikael Carlavan <contact@mika-carl.fr>
2020-07-16 22:37:24 +0200

    Update PDFDocEncodedStringType.php

--- src/Type/PDFDocEncodedStringType.php
+++ src/Type/PDFDocEncodedStringType.php
@@ -6,5 +6,14 @@ use Papier\Type\StringType;
 
 class PDFDocEncodedStringType extends StringType
 {
- 
+     /**
+     * Get object's value.
+     *
+     * @return string
+     */
+    protected function getValue()
+    {
+        $value = parent::getValue();
+        return mb_convert_encoding($value, 'ISO-8859-1');
+    }
 }
