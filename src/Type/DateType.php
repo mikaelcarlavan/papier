f386d1d75a0cc665afc1717099411368ec21d1ed
Mikael Carlavan <contact@mika-carl.fr>
2020-07-16 22:37:14 +0200

    Update DateType.php

---
+++ src/Type/DateType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Papier\Type;
+
+use Papier\Type\StringType;
+use DateTime;
+
+class DateType extends StringType
+{
+    /**
+    * Set object's value.
+    *
+    * @param  mixed  $date
+    * @throws InvalidArgumentException if the provided argument is not of type 'DateTime'.
+    * @return \Papier\Type\DateType
+    */
+    public function setValue($date)
+    {
+        if (!$date instanceof DateTime) {
+            throw new InvalidArgumentException("Date is incorrect. See ".get_class($this)." class's documentation for possible values.");
+        }
+
+        return parent::setValue($value);
+    } 
+
+     /**
+     * Get object's value.
+     *
+     * @return string
+     */
+    protected function getValue()
+    {
+        $value = parent::getValue();
+        $value = 'D:'.$value->format('YmdHis');
+        return $value;
+    }
+}
