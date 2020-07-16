b6700772eb897ecd7dae3da12e89370399457559
Mikael Carlavan <contact@mika-carl.fr>
2020-07-16 22:37:38 +0200

    Update TextStreamType.php

--- src/Type/TextStreamType.php
+++ src/Type/TextStreamType.php
@@ -5,7 +5,16 @@ namespace Papier\Type;
 use Papier\Type\StringType;
 use Papier\Object\StreamObject;
 
-class TextStringType extends StreamObject
+class TextStreamType extends StreamObject
 {
- 
+     /**
+     * Get object's content.
+     *  
+     * @return string
+     */
+    protected function getContent()
+    {
+        $content = parent::getcontent();
+        return mb_convert_encoding($content, 'UTF-16BE');
+    } 
 }
