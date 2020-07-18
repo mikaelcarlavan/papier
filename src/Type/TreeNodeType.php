7375b1922100a14dd52af423166a7594b5ab893e
Mikael Carlavan <contact@mika-carl.fr>
2020-07-18 11:59:18 +0200

    Update TreeNodeType.php

--- src/Type/TreeNodeType.php
+++ src/Type/TreeNodeType.php
@@ -9,21 +9,27 @@ use InvalidArgumentException;
 use RunTimeException;
 
 class TreeNodeType extends DictionaryObject
-{
-
+{ 
     /**
-     * Add entry to tree node's dictionnary.
-     *      
-     * @param  string  $key
-     * @param  mixed  $object
-     * @return \Papier\Document\DocumentCatalog
+     * Get kids.
+     *  
+     * @throws RunTimeException if node already contains 'Names' key.
+     * @return \Papier\Object\ArrayObject
      */
-    private function addEntry($key, $object)
+    public function getKids()
     {
-        $this->setObjectForKey($key, $object);
-        return $this;
-    } 
- 
+        if ($this->hasKey('Names')) {
+            throw new RunTimeException("Names is already present. See ".get_class($this)." class's documentation for possible values.");  
+        }
+
+        if (!$this->hasKey('Kids')) {
+            $kids = new ArrayObject();
+            $this->setObjectForKey('Kids', $kids);
+        }
+
+        return $this->getObjectForKey('Kids');
+    }
+
     /**
      * Set kids.
      *  
@@ -42,7 +48,7 @@ class TreeNodeType extends DictionaryObject
             throw new RunTimeException("Names is already present. See ".get_class($this)." class's documentation for possible values.");  
         }
 
-        return $this->addEntry('Kids', $kids);
+        return $this->setObjectForKey('Kids', $kids);
     }
     
     /**
@@ -63,8 +69,28 @@ class TreeNodeType extends DictionaryObject
             throw new RunTimeException("Kids is already present. See ".get_class($this)." class's documentation for possible values.");  
         }
 
-        return $this->addEntry('Names', $names);
+        return $this->setObjectForKey('Names', $names);
     } 
+
+    /**
+     * Get kids.
+     *  
+     * @throws RunTimeException if node already contains 'Names' key.
+     * @return \Papier\Object\ArrayObject
+     */
+    public function getNames()
+    {
+        if ($this->hasKey('Kids')) {
+            throw new RunTimeException("Kids is already present. See ".get_class($this)." class's documentation for possible values.");  
+        }
+
+        if (!$this->hasKey('Names')) {
+            $names = new ArrayObject();
+            $this->setObjectForKey('Names', $names);
+        }
+
+        return $this->getObjectForKey('Names');
+    }
 
     /**
      * Set limits.
@@ -74,12 +100,66 @@ class TreeNodeType extends DictionaryObject
      * @throws RunTimeException if node already contains 'Names' key.
      * @return \Papier\Document\DocumentCatalog
      */
-    public function setLimits($limits)
+    private function setLimits($limits)
     {
         if (!$limits instanceof ArrayObject) {
             throw new InvalidArgumentException("Limits is incorrect. See ".get_class($this)." class's documentation for possible values.");
         }
 
-        return $this->addEntry('Limits', $limits);
+        return $this->setObjectForKey('Limits', $limits);
     } 
+
+
+    /**
+     * Format object's value.
+     *
+     * @return string
+     */
+    public function format()
+    {
+        // Compute limits
+        $limits = new ArrayObject();
+        $objects = array();
+
+        if ($this->hasKey('Kids')) {
+            //Intermediate nodes
+            $kids = $this->getKids();
+
+            foreach ($kids as $kid) {
+                while ($kid instanceof TreeNodeType && $kid->hasKey('Kids')) {
+                    $kid = $kid->getKids();
+                }
+
+                if ($kid->hasKey('Names')) {
+                    $names = $kid['Names'];
+
+                    if (count($names) > 0) {
+                        for ($i=0; $i < count($names); $i+2) {
+                            $objects[] = $names[$i]->format();
+                        }
+                    }
+                }
+            }
+        } else if ($this->hasKey('Names')) {
+            $names = $this->getObjectForKey('Names');
+
+            if (count($names) > 0) {
+                for ($i=0; $i < count($names); $i+2) {
+                    $objects[] = $names[$i]->format();
+                }
+            }         
+        }
+
+        if (count($objects)) {
+            sort($objects);
+
+            $limits->append(array_shift($objects));
+            $limits->append(array_pop($objects));
+    
+            $this->setLimits($limits);
+        }
+
+
+        return parent::format();
+    }
 }
