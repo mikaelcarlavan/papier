9378ee325836453f7b341181a3c151ab05043a1a
Mikael Carlavan <contact@mika-carl.fr>
2020-08-08 09:21:30 +0200

    Update TreeNodeType.php

--- src/Type/TreeNodeType.php
+++ src/Type/TreeNodeType.php
@@ -4,6 +4,9 @@ namespace Papier\Type;
 
 use Papier\Object\DictionaryObject;
 use Papier\Object\ArrayObject;
+use Papier\Object\LiteralStringKeyArrayObject;
+use Papier\Object\LiteralStringObject;
+use Papier\Object\LimitsArrayObject;
 
 use InvalidArgumentException;
 use RunTimeException;
@@ -19,7 +22,7 @@ class TreeNodeType extends DictionaryObject
     public function getKids()
     {
         if ($this->hasKey('Names')) {
-            throw new RunTimeException("Names is already present. See ".get_class($this)." class's documentation for possible values.");  
+            throw new RunTimeException("Names is already present. See ".__CLASS__." class's documentation for possible values.");  
         }
 
         if (!$this->hasKey('Kids')) {
@@ -41,11 +44,11 @@ class TreeNodeType extends DictionaryObject
     public function setKids($kids)
     {
         if (!$kids instanceof ArrayObject) {
-            throw new InvalidArgumentException("Kids is incorrect. See ".get_class($this)." class's documentation for possible values.");
+            throw new InvalidArgumentException("Kids is incorrect. See ".__CLASS__." class's documentation for possible values.");
         }
 
         if ($this->hasKey('Names')) {
-            throw new RunTimeException("Names is already present. See ".get_class($this)." class's documentation for possible values.");  
+            throw new RunTimeException("Names is already present. See ".__CLASS__." class's documentation for possible values.");  
         }
 
         return $this->setObjectForKey('Kids', $kids);
@@ -54,19 +57,19 @@ class TreeNodeType extends DictionaryObject
     /**
      * Set names.
      *  
-     * @param  \Papier\Object\ArrayObject  $names
+     * @param  \Papier\Object\StringKeyArrayObject  $names
      * @throws InvalidArgumentException if the provided argument is not of type 'ArrayObject'.
      * @throws RunTimeException if node already contains 'Names' key.
      * @return \Papier\Document\DocumentCatalog
      */
     public function setNames($names)
     {
-        if (!$names instanceof ArrayObject) {
-            throw new InvalidArgumentException("Names is incorrect. See ".get_class($this)." class's documentation for possible values.");
+        if (!$names instanceof StringKeyArrayObject) {
+            throw new InvalidArgumentException("Names is incorrect. See ".__CLASS__." class's documentation for possible values.");
         }
 
         if ($this->hasKey('Kids')) {
-            throw new RunTimeException("Kids is already present. See ".get_class($this)." class's documentation for possible values.");  
+            throw new RunTimeException("Kids is already present. See ".__CLASS__." class's documentation for possible values.");  
         }
 
         return $this->setObjectForKey('Names', $names);
@@ -76,16 +79,16 @@ class TreeNodeType extends DictionaryObject
      * Get kids.
      *  
      * @throws RunTimeException if node already contains 'Names' key.
-     * @return \Papier\Object\ArrayObject
+     * @return \Papier\Object\LiteralStringKeyArrayObject
      */
     public function getNames()
     {
         if ($this->hasKey('Kids')) {
-            throw new RunTimeException("Kids is already present. See ".get_class($this)." class's documentation for possible values.");  
+            throw new RunTimeException("Kids is already present. See ".__CLASS__." class's documentation for possible values.");  
         }
 
         if (!$this->hasKey('Names')) {
-            $names = new ArrayObject();
+            $names = new LiteralStringKeyArrayObject();
             $this->setObjectForKey('Names', $names);
         }
 
@@ -95,15 +98,15 @@ class TreeNodeType extends DictionaryObject
     /**
      * Set limits.
      *  
-     * @param  \Papier\Object\ArrayObject  $limits
+     * @param  \Papier\Object\LimitsArrayObject  $limits
      * @throws InvalidArgumentException if the provided argument is not of type 'ArrayObject'.
      * @throws RunTimeException if node already contains 'Names' key.
      * @return \Papier\Document\DocumentCatalog
      */
     private function setLimits($limits)
     {
-        if (!$limits instanceof ArrayObject) {
-            throw new InvalidArgumentException("Limits is incorrect. See ".get_class($this)." class's documentation for possible values.");
+        if (!$limits instanceof LimitsArrayObject) {
+            throw new InvalidArgumentException("Limits is incorrect. See ".__CLASS__." class's documentation for possible values.");
         }
 
         return $this->setObjectForKey('Limits', $limits);
@@ -118,7 +121,7 @@ class TreeNodeType extends DictionaryObject
     public function format()
     {
         // Compute limits
-        $limits = new ArrayObject();
+        $limits = new LimitsArrayObject();
         $objects = array();
 
         if ($this->hasKey('Kids')) {
@@ -126,10 +129,11 @@ class TreeNodeType extends DictionaryObject
             $kids = $this->getKids();
 
             foreach ($kids as $kid) {
+                                
                 while ($kid instanceof TreeNodeType && $kid->hasKey('Kids')) {
                     $kid = $kid->getKids();
                 }
-
+                
                 if ($kid->hasKey('Names')) {
                     $names = $kid['Names'];
 
@@ -142,24 +146,29 @@ class TreeNodeType extends DictionaryObject
             }
         } else if ($this->hasKey('Names')) {
             $names = $this->getObjectForKey('Names');
-
-            if (count($names) > 0) {
-                for ($i=0; $i < count($names); $i+2) {
-                    $objects[] = $names[$i]->format();
-                }
-            }         
+            $objects += $names->getKeys();       
         }
 
         if (count($objects)) {
             sort($objects);
 
-            $limits->append(array_shift($objects));
-            $limits->append(array_pop($objects));
+            $first = new LiteralStringObject();
+            $last = new LiteralStringObject();
+                        
+            $first->setValue(array_shift($objects));
+            $last->setValue(array_pop($objects));
+            
+            $limits->append($first);
+            $limits->append($last);
     
             $this->setLimits($limits);
         }
 
-
+        $value = $this->getValue();
+        asort($value);
+        
+        $this->setValue($value);
+        
         return parent::format();
     }
 }
