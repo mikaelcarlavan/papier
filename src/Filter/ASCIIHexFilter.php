b6ebe3d3c791c1b943b394624c800d8f8f5c47d2
Mikael Carlavan <contact@mika-carl.fr>
2020-08-08 09:21:38 +0200

    Update ASCIIHexFilter.php

--- src/Filter/ASCIIHexFilter.php
+++ src/Filter/ASCIIHexFilter.php
@@ -3,17 +3,57 @@
 namespace Papier\Filter;
 
 use Papier\Filter\Base\Filter;
+use Papier\Validator\StringValidator;
+
+use RuntimeException;
+use InvalidArgumentException;
 
 class ASCIIHexFilter extends Filter
-{    
+{  
+    /**
+     * End-of-data marker.
+     *
+     * @var string
+     */
+    const EOD_MARKER = ">"; 
+
     /**
      * Decode stream.
      *  
      * @param  string  $stream
+     * @param  array  $param
      * @return string
      */
-    public function decode($stream)
+    public static function decode($stream, $param = array())
     {
-        return hex2bin($stream);
+        $stream = trim($stream);
+        $marker = substr($stream, -strlen(self::EOD_MARKER));
+
+        if ($marker != self::EOD_MARKER) {
+            throw new InvalidArgumentException("Stream is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $stream = substr($stream, 0, -strlen(self::EOD_MARKER));
+
+        return hex2bin($steam);
+    }
+
+    /**
+     * Encode value.
+     *  
+     * @param  string  $value
+     * @param  array  $param
+     * @return string
+     */
+    public static function encode($value, $param = array())
+    {
+        // Clean white-spaces
+        $value = preg_replace('/\s+/', '', $value);
+
+        if (!StringValidator::isValid($value)) {
+            throw new InvalidArgumentException("Value is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        return bin2hex($value) . self::EOD_MARKER;
     }
 }
