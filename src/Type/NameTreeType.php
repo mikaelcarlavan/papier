5fed6bc644c07c53609ec544cc97248229ec47f8
Mikael Carlavan <contact@mika-carl.fr>
2020-08-09 10:26:40 +0200

    Simplify code

--- src/Type/NameTreeType.php
+++ src/Type/NameTreeType.php
@@ -2,9 +2,36 @@
 
 namespace Papier\Type;
 
-use Papier\Object\DictionaryObject;
+use Papier\Type\TreeType;
+use Papier\Factory\Factory;
 
-class NameTreeType extends DictionaryObject
-{
- 
+class NameTreeType extends TreeType
+{ 
+    /**
+     * Get root.
+     *  
+     * @return \Papier\Type\NameTreeNodeType
+     */
+    public function getRoot()
+    {
+        if (!$this->hasKey('Root')) {
+            $root = Factory::getInstance()->createType('NameTreeNode')->setRoot();
+            $this->setObjectForKey('Root', $root);
+        }
+
+        return $this->getObjectForKey('Root');
+    }
+    
+    /**
+     * Add number to tree.
+     *  
+     * @param  mixed  $object
+     * @param  string  $key
+     * @return \Papier\Type\NameTreeType
+     */
+    public function addName($key, $object)
+    {
+        $this->getRoot()->addName($key, $object);
+        return $this;
+    }
 }
--- src/Type/NumberTreeType.php
+++ src/Type/NumberTreeType.php
@@ -2,9 +2,36 @@
 
 namespace Papier\Type;
 
-use Papier\Object\ArrayObject;
+use Papier\Type\TreeType;
+use Papier\Factory\Factory;
 
-class NumberTreeType extends ArrayObject
-{
- 
+class NumberTreeType extends TreeType
+{ 
+    /**
+     * Get root.
+     *  
+     * @return \Papier\Type\NumberTreeNodeType
+     */
+    public function getRoot()
+    {
+        if (!$this->hasKey('Root')) {
+            $root = Factory::getInstance()->createType('NumberTreeNode')->setRoot();
+            $this->setObjectForKey('Root', $root);
+        }
+
+        return $this->getObjectForKey('Root');
+    }
+
+    /**
+     * Add number to tree.
+     *  
+     * @param  mixed  $object
+     * @param  string  $key
+     * @return \Papier\Type\NumberTreeType
+     */
+    public function addNum($key, $object)
+    {
+        $this->getRoot()->addNum($key, $object);
+        return $this;
+    }
 }
