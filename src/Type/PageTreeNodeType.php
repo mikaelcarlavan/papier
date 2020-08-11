7f8459e5e2d9be046c611a46fb931d002fd745a4
Mikael Carlavan <contact@mika-carl.fr>
2020-08-11 22:11:02 +0200

    Update PageTreeNodeType.php

--- src/Type/PageTreeNodeType.php
+++ src/Type/PageTreeNodeType.php
@@ -2,62 +2,99 @@
 
 namespace Papier\Type;
 
-use Papier\Document\PageTree\PageTreeObject;
+use Papier\Type\PageObjectType;
 use Papier\Object\DictionaryObject;
+
+use Papier\Factory\Factory;
 
 use InvalidArgumentException;
 use RuntimeException;
 
-class PageTreeNodeType extends ArrayObject
+class PageTreeNodeType extends DictionaryObject
 {
-
-    /**
-     * Parent of tree node.
-     *
-     * @var \Papier\Object\DictionaryObject
-     */
-    private $parent = null;
-
     /**
      * Set parent.
      *  
-     * @param  \Papier\Object\DictionaryObject  $parent
-     * @throws InvalidArgumentException if the provided argument is not of type 'DictionaryObject'.
-     * @return \Document\PageTree\PageTreeNode
+     * @param  \Papier\Type\PageTreeNodeType  $parent
+     * @throws InvalidArgumentException if the provided argument is not of type 'PageTreeNodeType'.
+     * @return \Papier\Type\PageTreeNodeType
      */
     public function setParent($parent)
     {
-        if (!$parent instanceof DictionaryObject) {
+        if (!$parent instanceof PageTreeNodeType) {
             throw new InvalidArgumentException("Parent is incorrect. See ".__CLASS__." class's documentation for possible values.");
         }
 
-        $this->parent = $parent;
+        $this->setObjectForKey('Parent', $parent);
         return $this;
     } 
 
      /**
      * Get parent.
      *  
-     * @return mixed
+     * @return \Papier\Type\PageTreeNodeType
      */
     public function getParent()
     {
-        return $this->parent;
+        return $this->getObjectForKey('Parent');
     } 
 
     /**
-     * Add new page tree object.
+     * Get kids.
      *  
-     * @return \Document\PageTree\PageTreeObject
+     * @return \Papier\Object\ArrayObject
+     */
+    protected function getKids()
+    {
+        if (!$this->hasKey('Kids')) {
+            $kids = new ArrayObject();
+            $this->setObjectForKey('Kids', $kids);
+        }
+
+        return $this->getObjectForKey('Kids');
+    }
+
+    
+    /**
+     * Add node to tree.
+     *  
+     * @return \Papier\Type\PageTreeNode
+     */
+    public function addNode()
+    {
+        $node = Factory::getInstance()->createType('PageTreeNode');
+        $this->getKids()->append($node);
+
+        return $node;
+    }
+   
+    /**
+     * Add object to node.
+     *  
+     * @return \Papier\Type\PageObjectType
      */
     public function addObject()
     {
-        $object = new PageTreeObject();
-        $object->setIndirect(true);
+        $node = Factory::getInstance()->createType('PageObject');
+        $this->getKids()->append($node);
 
-        $this->append($object);
+        return $node;
+    }
 
-        return $object;
+    /**
+     * Set kids.
+     *  
+     * @param  \Papier\Object\ArrayObject  $kids
+     * @throws InvalidArgumentException if the provided argument is not of type 'ArrayObject'.
+     * @return \Papier\Type\TreeNodeType
+     */
+    protected function setKids($kids)
+    {
+        if (!$kids instanceof ArrayObject) {
+            throw new InvalidArgumentException("Kids is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        return $this->setObjectForKey('Kids', $kids);
     }
 
     /**
@@ -68,25 +105,41 @@ class PageTreeNodeType extends ArrayObject
     public function format()
     {
         $parent = $this->getParent();
-        $objects = $this->getObjects();
 
-        $type = new NameObject();
-        $type->setValue('Pages');
-
-        $num = new IntegerObject();
-        $num->setValue(count($objects));
-
-        if (!$parent) {
+        if (!$parent instanceof PageTreeNodeType) {
             throw new RuntimeException("Parent is missing. See ".__CLASS__." class's documentation for possible values.");
         }
 
-        $dictionary = new DictionaryObject();
-        $dictionary->setObjectForKey('Type', $type);
-        $dictionary->setObjectForKey('Parent', $parent);
-        $dictionary->setObjectForKey('Kids', $objects);
-        $dictionary->setObjectForKey('Count', $num);
+        $kids = $this->getKids();
+
+        $num = count($kids);
+
+        if ($num > 0) {
+            $kid = $kids->first();
+
+            $allObjects = $kid instanceof PageObjectType;
+            $allNodes = $kid instanceof PageTreeNodeType;
+
+            foreach ($kids as $kid) {
+                if ($kid instanceof PageTreeNodeType && $allObjects) {
+                    throw new RuntimeException("All kids should be of same type. See ".__CLASS__." class's documentation for possible values.");  
+                }
+
+                if ($kid instanceof PageObjectType && $allNodes) {
+                    throw new RuntimeException("All kids should be of same type. See ".__CLASS__." class's documentation for possible values.");  
+                }
+            }
+        }
         
-        $value = $dictionary->write();
-        return $value;
+        $type = Factory::getInstance()->createObject('Name')->setIndirect(false);
+        $type->setValue('Pages');
+
+        $count = Factory::getInstance()->createObject('Integer')->setIndirect(false);
+        $count->setValue($num);
+
+        $this->setObjectForKey('Type', $type);
+        $this->setObjectForKey('Count', $count);
+        
+        return parent::format();
     }
 }
