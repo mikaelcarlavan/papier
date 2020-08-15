a38cd238e309b61f9bf9b2d15e8bb2ae2088b2a2
Mikael Carlavan <contact@mika-carl.fr>
2020-08-15 16:31:51 +0200

    Create CrossReference.php

---
+++ src/File/CrossReference.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Papier\File;
+
+use Papier\Object\DictionaryObject;
+use Papier\Base\IndirectObject;
+
+use Papier\File\CrossReferenceTable;
+
+use Papier\Validator\IntValidator;
+
+use InvalidArgumentException;
+
+class CrossReference extends DictionaryObject
+{
+    /**
+     * Offset of the crossreference.
+     *
+     * @var int
+     */
+    protected $offset = 0;
+
+    /**
+    * Instance of the object.
+    *
+    * @var \Papier\File\CrossReference
+    */
+    protected static $instance = null;
+   
+    /**
+    * Get instance of crossreference.
+    *
+    * @return \Papier\File\CrossReference
+    */
+    public static function getInstance() 
+    {
+        if(is_null(self::$instance)) {
+            self::$instance = new CrossReference();  
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Set crossreference's offset.
+     *  
+     * @param  int  $offset
+     * @throws InvalidArgumentException if the provided argument is not of type 'int'.
+     * @return \Papier\File\CrossReference
+     */
+    public function setOffset($offset)
+    {
+        if (!IntValidator::isValid($offset, 0)) {
+            throw new InvalidArgumentException("Offset is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $this->offset = $offset;
+        return $this;
+    } 
+
+    /**
+     * Get entry's offset.
+     *
+     * @return int
+     */
+    protected function getOffset()
+    {
+        return $this->offset;
+    }
+
+    /**
+     * Add object to crossreference.
+     *  
+     * @param  \Papier\Base\IndirectObject  $object
+     * @return \Papier\File\CrossReference
+     */
+    public function addObject($object)
+    {        
+        if (!$object instanceof IndirectObject) {
+            throw new InvalidArgumentException("Object is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $objects = $this->getObjects();
+        $objects[$object->getNumber()] = $object;
+
+        return $this->setObjects($objects);
+    }
+
+    /**
+     * Remove object from crossreference.
+     *  
+     * @param  \Papier\Base\IndirectObject  $object
+     * @return \Papier\File\CrossReference
+     */
+    public function removeObject($object)
+    {
+        if (!$object instanceof IndirectObject) {
+            throw new InvalidArgumentException("Object is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $objects = $this->getObjects();
+        unset($objects[$object->getNumber()]);
+
+        return $this->setObjects($objects);
+    }
+
+    /**
+     * Format object's value.
+     *
+     * @return string
+     */
+    public function format()
+    {
+        $table = new CrossReferenceTable();
+        $subsection = $table->addSection()->addSubsection();
+
+        $subsection->addEntry()->setFree()->setGeneration(65535);
+
+        $offset = $this->getOffset();
+
+        $objects = $this->getObjects();
+        if (count($objects) > 0) {
+            foreach ($objects as $object) {
+                $subsection->addEntry()->setFree(false)->setOffset($offset);
+                $offset += strlen($object->getObject());
+            }
+        }
+
+        return $table->write();
+    }
+}
