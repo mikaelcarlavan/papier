cea78ceb3402519502ecb98d8be4774ecea1d7b6
Mikael Carlavan <contact@mika-carl.fr>
2020-08-16 22:34:22 +0200

    Create PostScriptCalculatorFunctionType.php

---
+++ src/Type/PostScriptCalculatorFunctionType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Papier\Type;
+
+use Papier\Object\FunctionObject;
+use Papier\Object\IntegerObject;
+use Papier\Object\ArrayObject;
+
+use Papier\Functions\FunctionType;
+
+use Papier\Validator\BitsPerSampleValidator;
+use Papier\Validator\IntValidator;
+
+use InvalidArgumentException;
+use RuntimeException;
+
+class PostScriptCalculatorFunctionType extends FunctionObject
+{
+    /**
+     * Format object's value.
+     *
+     * @return string
+     */
+    public function format()
+    {
+        $this->setFunctionType(FunctionType::POSTSCRIPT_CALCULATOR);
+
+        return parent::format();
+    }
+}
