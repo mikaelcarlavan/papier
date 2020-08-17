5e162faa4ba560bb34fb0399dbe1ad2cf2cdfec0
Mikael Carlavan <contact@mika-carl.fr>
2020-08-17 18:34:03 +0200

    Create LiteralStringKeyArrayType.php

---
+++ src/Type/LiteralStringKeyArrayType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Papier\Type;
+
+use Papier\Object\DictionaryObject;
+use Papier\Base\IndirectObject;
+
+use Papier\Factory\Factory;
+
+use InvalidArgumentException;
+
+class LiteralStringKeyArrayType extends DictionaryObject
+{
+
+    /**
+     * Format object's value.
+     *
+     * @return string
+     */
+    public function format()
+    {
+        $objects = $this->getObjects();
+        
+        $value = '';
+        if (is_array($objects)) {
+            foreach ($objects as $key => $object) {
+                $name = Factory::create('LiteralString', $key, false);
+                $value .= $name->format() .' '. $object->write();
+            }
+        }
+
+        return '[' .$value. ']';
+    }
+}
