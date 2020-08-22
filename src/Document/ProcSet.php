a4b72887d806ac0d443db75e7e6bca81a4402dd9
Mikael Carlavan <contact@mika-carl.fr>
2020-08-22 22:34:33 +0200

    Create ProcSet.php

---
+++ src/Document/ProcSet.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Papier\Document;
+
+class ProcSet
+{
+    /**
+     * Painting and graphics state
+     *
+     * @var string
+     */
+    const GRAPHICS = 'PDF';
+  
+    /**
+     * Text
+     *
+     * @var string
+     */
+    const TEXT = 'Text';
+
+    /**
+     * Indexed (colour-table) images
+     *
+     * @var string
+     */
+    const INDEXED_IMAGES = 'ImageI';
+
+    /**
+     * Colour images
+     *
+     * @var string
+     */
+    const COLOUR_IMAGES = 'ImageC';
+
+    /**
+     * Grayscale images or image masks
+     *
+     * @var string
+     */
+    const GRAYSCALE_IMAGES = 'ImageB';
+}
