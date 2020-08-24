09c899d73094bae2612b05e73eaa0c5274c03da4
Mikael Carlavan <contact@mika-carl.fr>
2020-08-24 22:08:13 +0200

    Update DocumentInformationDictionaryType.php

--- src/Type/DocumentInformationDictionaryType.php
+++ src/Type/DocumentInformationDictionaryType.php
@@ -8,6 +8,8 @@ use Papier\Object\ArrayObject;
 use Papier\Factory\Factory;
 
 use Papier\Validator\StringValidator;
+use Papier\Validator\DateValidator;
+use Papier\Validator\TrappedValidator;
 
 use InvalidArgumentException;
 
@@ -29,7 +31,7 @@ class DocumentInformationDictionaryType extends DictionaryObject
         $value = Factory::create('TextString', $title);
 
         $this->setEntry('Title', $value);
-        return $extension;
+        return $this;
     } 
 
     /**
@@ -48,7 +50,7 @@ class DocumentInformationDictionaryType extends DictionaryObject
         $value = Factory::create('TextString', $author);
 
         $this->setEntry('Author', $value);
-        return $extension;
+        return $this;
     } 
 
     /**
@@ -67,7 +69,7 @@ class DocumentInformationDictionaryType extends DictionaryObject
         $value = Factory::create('TextString', $subject);
 
         $this->setEntry('Subject', $value);
-        return $extension;
+        return $this;
     } 
 
     /**
@@ -86,6 +88,101 @@ class DocumentInformationDictionaryType extends DictionaryObject
         $value = Factory::create('TextString', $keywords);
 
         $this->setEntry('Keywords', $value);
-        return $extension;
+        return $this;
     } 
+
+    /**
+     * Set creator.
+     * 
+     * @param   string  $creator
+     * @throws InvalidArgumentException if the provided argument is not of type 'string'.
+     * @return \Papier\Type\DocumentInformationDictionaryType
+     */
+    public function setCreator($creator)
+    {
+        if (!StringValidator::isValid($creator)) {
+            throw new InvalidArgumentException("Creator is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $value = Factory::create('TextString', $creator);
+
+        $this->setEntry('Creator', $value);
+        return $this;
+    } 
+
+    /**
+     * Set producer.
+     * 
+     * @param   string  $producer
+     * @throws InvalidArgumentException if the provided argument is not of type 'string'.
+     * @return \Papier\Type\DocumentInformationDictionaryType
+     */
+    public function setProducer($producer)
+    {
+        if (!StringValidator::isValid($producer)) {
+            throw new InvalidArgumentException("Producer is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $value = Factory::create('TextString', $producer);
+
+        $this->setEntry('Producer', $value);
+        return $this;
+    } 
+
+    /**
+     * Set creation date.
+     * 
+     * @param   string  $producer
+     * @throws InvalidArgumentException if the provided argument is not a valid date.
+     * @return \Papier\Type\DocumentInformationDictionaryType
+     */
+    public function setCreationDate($date)
+    {
+        if (!DateValidator::isValid($date)) {
+            throw new InvalidArgumentException("CreationDate is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $value = Factory::create('Date', $date);
+
+        $this->setEntry('CreationDate', $value);
+        return $this;
+    }
+
+    /**
+     * Set modification date.
+     * 
+     * @param   string  $producer
+     * @throws InvalidArgumentException if the provided argument is not a valid date.
+     * @return \Papier\Type\DocumentInformationDictionaryType
+     */
+    public function setModDate($date)
+    {
+        if (!DateValidator::isValid($date)) {
+            throw new InvalidArgumentException("ModDate is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $value = Factory::create('TextString', $date);
+
+        $this->setEntry('ModDate', $value);
+        return $this;
+    }
+
+    /**
+     * Set trapped.
+     * 
+     * @param   string  $trapped
+     * @throws InvalidArgumentException if the provided argument is not a valid trapped.
+     * @return \Papier\Type\DocumentInformationDictionaryType
+     */
+    public function setTrapped($trapped)
+    {
+        if (!TrappedValidator::isValid($trapped)) {
+            throw new InvalidArgumentException("Trapped is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $value = Factory::create('Name', $trapped);
+
+        $this->setEntry('Trapped', $value);
+        return $this;
+    }
 }
