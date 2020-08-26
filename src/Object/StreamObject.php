3c8990674e08616bd52b74e871a0744725f4a9ce
Mikael Carlavan <contact@mika-carl.fr>
2020-08-26 21:45:35 +0200

    Update StreamObject.php

--- src/Object/StreamObject.php
+++ src/Object/StreamObject.php
@@ -7,6 +7,8 @@ use Papier\Object\DictionaryObject;
 
 use Papier\Factory\Factory;
 
+use Papier\Validator\StringValidator;
+
 use RunTimeException;
 use InvalidArgumentException;
 
@@ -86,6 +88,98 @@ class StreamObject extends DictionaryObject
 
         return $this->setContent($content);
     } 
+
+    /**
+     * Set filter.
+     *  
+     * @param  mixed  $filter
+     * @throws InvalidArgumentException if the provided argument is not of type 'string' or 'ArrayObject'.
+     * @return \Papier\Object\StreamObject
+     */
+    public function setFilter($filter)
+    {
+        if (!StringValidator::isValid($filter) && !$filter instanceof ArrayObject) {
+            throw new InvalidArgumentException("Filter is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $value = $filter instanceof ArrayObject ? $filter : Factory::create('Name', $filter);
+
+        $this->setEntry('Filter', $value);
+        return $this;
+    } 
+
+    /**
+     * Set decode parameters.
+     *  
+     * @param  mixed  $parms
+     * @throws InvalidArgumentException if the provided argument is not of type 'DictionaryObject' or 'ArrayObject'.
+     * @return \Papier\Object\StreamObject
+     */
+    public function setDecodeParms($parms)
+    {
+        if (!$parms instanceof DictionaryObject && !$parms instanceof ArrayObject) {
+            throw new InvalidArgumentException("DecodeParms is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $this->setEntry('DecodeParms', $parms);
+        return $this;
+    } 
+
+    /**
+     * Set file filter.
+     *  
+     * @param  mixed  $ffilter
+     * @throws InvalidArgumentException if the provided argument is not of type 'string' or 'ArrayObject'.
+     * @return \Papier\Object\StreamObject
+     */
+    public function setFFilter($ffilter)
+    {
+        if (!StringValidator::isValid($ffilter) && !$ffilter instanceof ArrayObject) {
+            throw new InvalidArgumentException("FFilter is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $value = $ffilter instanceof ArrayObject ? $ffilter : Factory::create('Name', $ffilter);
+
+        $this->setEntry('FFilter', $value);
+        return $this;
+    } 
+
+    /**
+     * Set file decode parameters.
+     *  
+     * @param  mixed  $parms
+     * @throws InvalidArgumentException if the provided argument is not of type 'DictionaryObject' or 'ArrayObject'.
+     * @return \Papier\Object\StreamObject
+     */
+    public function setFDecodeParms($parms)
+    {
+        if (!$parms instanceof DictionaryObject && !$parms instanceof ArrayObject) {
+            throw new InvalidArgumentException("FDecodeParms is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $this->setEntry('FDecodeParms', $parms);
+        return $this;
+    } 
+
+    /**
+     * Set length of decoded stream.
+     *  
+     * @param  int  $dl
+     * @throws InvalidArgumentException if the provided argument is not of type 'int'.
+     * @return \Papier\Object\StreamObject
+     */
+    public function setDL($dl)
+    {
+        if (!IntegerValidator::isValid($dl)) {
+            throw new InvalidArgumentException("DL is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $value = Factory::create('Integer', $dl);
+
+        $this->setEntry('DL', $value);
+        return $this;
+    } 
+
     /**
      * Format object's value.
      *
@@ -94,7 +188,6 @@ class StreamObject extends DictionaryObject
     public function format()
     {
         $stream = $this->getStream();
-        //$stream = $stream ? $stream->write() : '';
 
         // Compute length of stream and set it into dictionary
         $length = Factory::create('Integer', intval(strlen($stream)));
