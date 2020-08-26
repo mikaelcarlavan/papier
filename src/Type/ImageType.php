68b6bba48276d4ccd5d46531df36cc4464c762b5
Mikael Carlavan <contact@mika-carl.fr>
2020-08-26 21:45:19 +0200

    Update ImageType.php

--- src/Type/ImageType.php
+++ src/Type/ImageType.php
@@ -2,6 +2,8 @@
 
 namespace Papier\Type;
 
+use Papier\Filter\FilterType;
+
 use Papier\Object\StreamObject;
 use Papier\Object\ArrayObject;
 use Papier\Object\IntegerObject;
@@ -151,6 +153,25 @@ class ImageType extends StreamObject
     }
 
     /**
+     * Set map from image samples to the appropriate ranges of values.
+     *  
+     * @param  array  $decode
+     * @throws InvalidArgumentException if the provided argument is not of type 'array' and if each element of the provided argument is not of type 'int' or 'float.
+     * @return \Papier\Type\ImageType
+     */
+    public function setDecode($decode)
+    {
+        if (!NumbersArrayValidator::isValid($decode)) {
+            throw new InvalidArgumentException("Decode is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $value = Factory::create('NumbersArray', $decode);
+
+        $this->setEntry('Decode', $value);
+        return $this;
+    }
+
+    /**
      * Set alternates.
      *  
      * @param  \Papier\Object\ArrayObject  $alternates
@@ -168,21 +189,163 @@ class ImageType extends StreamObject
     }
 
     /**
-     * Set map from image samples to the appropriate ranges of values.
+     * Set interpolation.
      *  
-     * @param  array  $decode
-     * @throws InvalidArgumentException if the provided argument is not of type 'array' and if each element of the provided argument is not of type 'int' or 'float.
+     * @param  bool  $interpolate
+     * @throws InvalidArgumentException if the provided argument does not inherit 'bool'.
      * @return \Papier\Type\ImageType
      */
-    public function setDecode($decode)
+    public function setInterpolate($interpolate = true)
     {
-        if (!NumbersArrayValidator::isValid($decode)) {
-            throw new InvalidArgumentException("Decode is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        if (!BooleanValidator::isValid($interpolate)) {
+            throw new InvalidArgumentException("Interpolate is incorrect. See ".__CLASS__." class's documentation for possible values.");
         }
 
-        $value = Factory::create('NumbersArray', $decode);
+        $value = Factory::create('Boolean', $interpolate);
 
-        $this->setEntry('Decode', $value);
+        $this->setEntry('Interpolate', $value);
+        return $this;
+    }
+
+    /**
+     * Set soft-mask image.
+     *  
+     * @param  \Papier\Object\StreamObject  $smask
+     * @throws InvalidArgumentException if the provided argument is not of type 'StreamObject'.
+     * @return \Papier\Type\ImageType
+     */
+    public function setSMask($smask)
+    {
+        if (!$smask instanceof StreamObject) {
+            throw new InvalidArgumentException("SMask is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $this->setEntry('SMask', $smask);
+        return $this;
+    }
+
+    /**
+     * Set soft-mask image.
+     *  
+     * @param  int  $indata
+     * @throws InvalidArgumentException if the provided argument is not of type 'int'.
+     * @return \Papier\Type\ImageType
+     */
+    public function setSMaskInData($indata)
+    {
+        if (!IntegerValidator::isValid($indata)) {
+            throw new InvalidArgumentException("SMaskInData is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $value = Factory::create('Integer', $indata);
+
+        $this->setEntry('SMaskInData', $value);
+        return $this;
+    }
+
+    /**
+     * Set name.
+     *  
+     * @param  mixed  $name
+     * @throws InvalidArgumentException if the provided argument is not of type 'string'.
+     * @return \Papier\Type\ImageType
+     */
+    public function setName($name)
+    {
+        if (!StringValidator::isValid($space)) {
+            throw new InvalidArgumentException("Name is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $value = Factory::create('Name', $name);
+
+        $this->setEntry('Name', $value);
+        return $this;
+    }
+
+    /**
+     * Set integer key of image's entry in the structural parent tree.
+     *  
+     * @param  int  $struct
+     * @throws InvalidArgumentException if the provided argument is not of type 'int'.
+     * @return \Papier\Type\ImageType
+     */
+    public function setStructParent($struct)
+    {
+        if (!IntegerValidator::isValid($struct)) {
+            throw new InvalidArgumentException("StructParent is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $value = Factory::create('Integer', $struct);
+
+        $this->setEntry('StructParent', $value);
+        return $this;
+    }
+
+    /**
+     * Set digital identifier of the page's parent web capture content set.
+     *  
+     * @param  \Papier\Type\ByteStringType  $id
+     * @throws InvalidArgumentException if the provided argument is not of type 'ByteStringType'.
+     * @return \Papier\Type\ImageType
+     */
+    public function setID($id)
+    {
+        if (!$id instanceof ByteStringType) {
+            throw new InvalidArgumentException("ID is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $this->setEntry('ID', $id);
+        return $this;
+    }
+
+    /**
+     * Set open prepress interface.
+     *  
+     * @param  \Papier\Object\DictionaryObject  $opi
+     * @throws InvalidArgumentException if the provided argument is not of type 'DictionaryObject'.
+     * @return \Papier\Type\ImageType
+     */
+    public function setOPI($opi)
+    {
+        if (!$opi instanceof DictionaryObject) {
+            throw new InvalidArgumentException("OPI is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $this->setEntry('OPI', $opi);
+        return $this;
+    }
+
+    /**
+     * Set metadata.
+     *  
+     * @param  \Papier\Object\StreamObject  $metadata
+     * @throws InvalidArgumentException if the provided argument is not of type 'StreamObject'.
+     * @return \Papier\Type\ImageType
+     */
+    public function setMetadata($metadata)
+    {
+        if (!$metadata instanceof StreamObject) {
+            throw new InvalidArgumentException("Metadata is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $this->setEntry('Metadata', $metadata);
+        return $this;
+    } 
+
+    /**
+     * Set optional content.
+     *  
+     * @param  \Papier\Object\DictionaryObject  $oc
+     * @throws InvalidArgumentException if the provided argument is not of type 'DictionaryObject'.
+     * @return \Papier\Type\ImageType
+     */
+    public function setOC($oc)
+    {
+        if (!$oc instanceof DictionaryObject) {
+            throw new InvalidArgumentException("OC is incorrect. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        $this->setEntry('OC', $oc);
         return $this;
     }
 
@@ -211,9 +374,27 @@ class ImageType extends StreamObject
             throw new RuntimeException("ColorSpace is incompatible. See ".__CLASS__." class's documentation for possible values.");
         }
 
-        if ($this->hasEntry('ImageMask') && $this->getEntry('ImageMask')->isTrue() && $this->hasEntry('Mask') ) {
+        if ($this->hasEntry('ImageMask') && $this->getEntry('ImageMask')->isTrue() && $this->hasEntry('Mask')) {
             throw new RuntimeException("Mask is not allowed. See ".__CLASS__." class's documentation for possible values.");
         }
+
+        if ($this->hasEntry('ImageMask') && $this->getEntry('ImageMask')->isTrue() && $this->hasEntry('BitsPerComponent') && $this->getEntry('BitsPerComponent')->getValue() == 1) {
+            throw new RuntimeException("BitsPerComponent is incompatible. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        /*
+        if ($this->hasEntry('Filter') && $this->getEntry('Filter')->has(FilterType::JPX_DECODE)) {
+            $this->unsetEntry('BitsPerComponent');
+        }
+
+        if ($this->hasEntry('Filter') && ($this->getEntry('Filter')->has(FilterType::CCITT_FAX_DECODE) || $this->getEntry('Filter')->has(FilterType::JBIG2_DECODE)) && $this->getEntry('BitsPerComponent')->getValue() == 1) {
+            throw new RuntimeException("BitsPerComponent is incompatible. See ".__CLASS__." class's documentation for possible values.");
+        }
+
+        if ($this->hasEntry('Filter') && ($this->getEntry('Filter')->has(FilterType::RUN_LENGTH_DECODE) || $this->getEntry('Filter')->has(FilterType::DCT_DECODE)) && $this->getEntry('BitsPerComponent')->getValue() == 8) {
+            throw new RuntimeException("BitsPerComponent is incompatible. See ".__CLASS__." class's documentation for possible values.");
+        }
+        */
 
         return parent::format();
     }
