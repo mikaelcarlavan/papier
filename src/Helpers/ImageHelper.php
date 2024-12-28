640bfd164adc61184c755fd2417e4d5e749a7af9
Mikael Carlavan <contact@mika-carl.fr>
2024-12-28 12:04:31 +0100

    Reformat code

--- src/Helpers/ImageHelper.php
+++ src/Helpers/ImageHelper.php
@@ -10,94 +10,94 @@ use InvalidArgumentException;
 
 class ImageHelper
 {
-    /**
-     * Read file and returns image colors and transparency.
-     *
-     * @param string $file
-     * @return array<mixed>
+	/**
+	 * Read file and returns image colors and transparency.
+	 *
+	 * @param string $file
+	 * @return array<mixed>
 	 */
-    public static function getDataFromSource(string $file): array
+	public static function getDataFromSource(string $file): array
 	{
-        $dimensions = getimagesize($file);
+		$dimensions = getimagesize($file);
 
-        $mime = $dimensions['mime'] ?? null;
+		$mime = $dimensions['mime'] ?? null;
 
 		$data = null;
 		$mask = null;
-        if ($mime == 'image/jpeg') {
-            $data = ImageHelper::getDataFromJPEG($file);
-        } else if ($mime == 'image/png') {
+		if ($mime == 'image/jpeg') {
+			$data = ImageHelper::getDataFromJPEG($file);
+		} else if ($mime == 'image/png') {
 			list($data, $mask) = ImageHelper::getDataFromPNG($file);
-        }
+		}
 
-        return array($data, $mask);
-    }
+		return array($data, $mask);
+	}
 
-    /**
-     * Read JPG/JPEG file and returns compressed data.
-     *
-     * @param string $file
-     * @return string|bool
-     */
-    public static function getDataFromJPEG(string $file): string|bool
-    {
-		return file_get_contents($file);
-    }
-
-    /**
-     * Read PNG file and returns compressed colors and transparency.
-     *
-     * @param string $file
-     * @return array<mixed>
+	/**
+	 * Read JPG/JPEG file and returns compressed data.
+	 *
+	 * @param string $file
+	 * @return string|bool
 	 */
-    public static function getDataFromPNG(string $file): array
+	public static function getDataFromJPEG(string $file): string|bool
 	{
-        $data = null;
+		return file_get_contents($file);
+	}
 
-        // Open
-        $stream = FileHelper::getInstance()->open($file);
+	/**
+	 * Read PNG file and returns compressed colors and transparency.
+	 *
+	 * @param string $file
+	 * @return array<mixed>
+	 */
+	public static function getDataFromPNG(string $file): array
+	{
+		$data = null;
+
+		// Open
+		$stream = FileHelper::getInstance()->open($file);
 		$stream->setBigEndian();
 
-        $header = $stream->read(8);
+		$header = $stream->read(8);
 
-        // PNG signature, check https://en.wikipedia.org/wiki/PNG
-        if ($header != "\x89\x50\x4e\x47\x0d\x0a\x1a\x0a") {
-            throw new InvalidArgumentException("Source is not a valid PNG file. See ".__CLASS__." class's documentation for possible values.");
-        }
+		// PNG signature, check https://en.wikipedia.org/wiki/PNG
+		if ($header != "\x89\x50\x4e\x47\x0d\x0a\x1a\x0a") {
+			throw new InvalidArgumentException("Source is not a valid PNG file. See " . __CLASS__ . " class's documentation for possible values.");
+		}
 
-        // Read first chunk (should be IHDR type)
-        $length = $stream->unpackUnsignedInteger(); // Should be equal to 13
-        $type = $stream->read(4);
-        if ($type != "IHDR") {
-            throw new InvalidArgumentException("Source is not a valid PNG file. See ".__CLASS__." class's documentation for possible values.");
-        }
+		// Read first chunk (should be IHDR type)
+		$length = $stream->unpackUnsignedInteger(); // Should be equal to 13
+		$type = $stream->read(4);
+		if ($type != "IHDR") {
+			throw new InvalidArgumentException("Source is not a valid PNG file. See " . __CLASS__ . " class's documentation for possible values.");
+		}
 
 		$width = $stream->unpackUnsignedInteger();
-        $height = $stream->unpackUnsignedInteger();
-        $bitDepth = $stream->unpackByte();
-        $colorType = $stream->unpackByte();
+		$height = $stream->unpackUnsignedInteger();
+		$bitDepth = $stream->unpackByte();
+		$colorType = $stream->unpackByte();
 
-        $compressionMethod = $stream->unpackByte();
-        $filterMethod = $stream->unpackByte();
-        $interlaceMethod = $stream->unpackByte();
+		$compressionMethod = $stream->unpackByte();
+		$filterMethod = $stream->unpackByte();
+		$interlaceMethod = $stream->unpackByte();
 
-        $crc = $stream->read(4);
+		$crc = $stream->read(4);
 		$pixels = $width * $height;
 
-        while ($type != 'IEND') {
-            $length = $stream->unpackUnsignedInteger();
-            $type = $stream->read(4);
+		while ($type != 'IEND') {
+			$length = $stream->unpackUnsignedInteger();
+			$type = $stream->read(4);
 
-            if ($type == 'IDAT') { // Data
-                $data .= $stream->read($length);
-            } else if ($length > 0) {
-                $stream->read($length);
-            }
+			if ($type == 'IDAT') { // Data
+				$data .= $stream->read($length);
+			} else if ($length > 0) {
+				$stream->read($length);
+			}
 
-            $crc = $stream->read(4);
-        }
+			$crc = $stream->read(4);
+		}
 
-        $stream->close();
+		$stream->close();
 
 		$mask = null;
 
@@ -132,6 +132,6 @@ class ImageHelper
 			}
 		}
 
-        return array($data, $mask);
-    }
+		return array($data, $mask);
+	}
 }
