c5d98affc3ebb743554a7b9c33e99376f2e9481d
Mikael Carlavan <contact@mika-carl.fr>
2025-05-25 18:59:02 +0200

    Create TextAlign.php

---
+++ src/Text/TextAlign.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Papier\Text;
+
+class TextAlign
+{
+	/**
+	 * Left align
+	 *
+	 * @var string
+	 */
+	const LEFT = 'L';
+
+	/**
+	 * Right align
+	 *
+	 * @var string
+	 */
+	const RIGHT = 'R';
+
+	/**
+	 * Center align
+	 *
+	 * @var string
+	 */
+	const CENTER = 'C';
+
+	/**
+	 * Justify align
+	 *
+	 * @var string
+	 */
+	const JUSTIFY = 'J';
+}
